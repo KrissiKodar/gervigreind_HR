62fa77feb2b3f567200e020ea3b2b171422b9201
KrissiTekkari <dadasonk@gmail.com>
2023-04-02 14:37:49 +0000

    sma breyting

--- project2/nonogram.py
+++ project2/nonogram.py
@@ -276,8 +276,8 @@ class backtracking_with_prechecking(nonogram_solver):
 						if k+1 < len(result) and result[k+1] == 0:
 							group_fill += 1	
 				padded_cells = self.pad_list(test, padding_list)
-				print("col", i)
-				print("padded_cells", padded_cells)
+				#print("col", i)
+				#print("padded_cells", padded_cells)
 				# where padded cells are 0 make self.assignment[(m, i)] = 0
 				for m, value in enumerate(padded_cells):
 					if value == 0:
@@ -372,11 +372,17 @@ class backtracking_with_prechecking(nonogram_solver):
 
 	def start_search(self, puzzle):
 		# where dictionary is not None assign values to puzzle
+		non_determined_cells = 0
 		for key, value in self.assignment.items():
 			if value != None:
 				puzzle[key] = value
+				continue
+			non_determined_cells += 1
 		print("before starting search:")
 		self.print_assignment(self.assignment)
+		print("size of old state space = ", "{:.2e}".format(2**(self.n_cells)))
+		print("non_determined_cells", non_determined_cells)
+		print("size of new state space = ", "{:.2e}".format(2**(non_determined_cells)))
 		print("------------------------")
 		return self.backtracking_search(puzzle)
 
@@ -413,7 +419,7 @@ class backtracking_with_prechecking(nonogram_solver):
 		# Order domain values
 		values_list = [1, 0]
 		#random.shuffle(values_list)
-		#alues_list = random.choices([[1, 0], [0, 1]], weights=[0.85, 0.15])[0]
+		#values_list = random.choices([[1, 0], [0, 1]], weights=[0.50, 0.50])[0]
 		for i in values_list:
 			temp_puzzle = puzzle.copy()
 			temp_puzzle[current_cell] = i
@@ -428,9 +434,8 @@ class backtracking_with_prechecking(nonogram_solver):
 		return False
 
 
-
-
 # backtracking with prechecking with added forward checking
+# cursed code (not working)
 class forward_checking(nonogram_solver):
 	def __init__(self, puzzle, row_constraints, col_constraints):
 		super().__init__(row_constraints, col_constraints)
@@ -536,8 +541,8 @@ class forward_checking(nonogram_solver):
 						if k+1 < len(result) and result[k+1] == 0:
 							group_fill += 1	
 				padded_cells = self.pad_list(test, padding_list)
-				print("col", i)
-				print("padded_cells", padded_cells)
+				#print("col", i)
+				#print("padded_cells", padded_cells)
 				# where padded cells are 0 make self.assignment[(m, i)] = 0
 				for m, value in enumerate(padded_cells):
 					if value == 0:
@@ -639,11 +644,16 @@ class forward_checking(nonogram_solver):
 
 	def start_search(self, puzzle):
 		# where dictionary is not None assign values to puzzle
+		non_determined_cells = 0
 		for key, value in self.assignment.items():
 			if value != None:
+				print("puzzle[key]", puzzle[key])
 				puzzle[key] = value
+				continue
+			non_determined_cells += 1
 		print("before starting search:")
 		self.print_assignment(self.assignment)
+		print("non_determined_cells", non_determined_cells)
 		print("------------------------")
 		return self.backtracking_search(puzzle)
 
@@ -710,6 +720,297 @@ class forward_checking(nonogram_solver):
 		return False
 
 
+# another attempt at forward checking
+class forward_checking_attempt_2(nonogram_solver):
+	def __init__(self, puzzle, row_constraints, col_constraints):
+		super().__init__(row_constraints, col_constraints)
+		self.n_rows = puzzle.shape[0]
+		self.n_cols = puzzle.shape[1]
+		self.n_cells = self.n_rows * self.n_cols
+     
+		self.row_constraints = row_constraints
+		self.row_sums = [np.sum(row) for row in self.row_constraints]
+		self.row_groups = [len(row) for row in self.row_constraints]
+  
+		self.col_constraints = col_constraints
+		self.col_sums = [np.sum(col) for col in self.col_constraints]
+		self.col_groups = [len(col) for col in self.col_constraints]
+  
+		self.assignments_before_search = {}
+		self.states_visited = 0
+
+		# pre-constrain check
+		for i in range(self.n_rows):
+			for j in range(self.n_cols):
+				self.assignments_before_search[(i, j)] = None
+     
+		# simple boxes
+		# simple boxes is a method explained here https://en.wikipedia.org/wiki/Nonogram
+		# it is a method which can solve parts of the puzzle before starting the search.
+		for i, row in enumerate(puzzle):
+			index = 0
+			block = 1
+			row_copy = row.copy()
+			for j in row_constraints[i]:
+				for k in range(j):
+					row_copy[index] = block
+					index += 1
+				index += 1
+				block += 1
+			for k in range(len(row_copy)-1, -1, -1):
+				if row_copy[k] != 0:
+					shift_by = len(row_copy) - k - 1
+					break
+			# shift list row by shift_by to the right
+			row_copy = list(row_copy)
+			row_shifted = row_copy[-shift_by:] + row_copy[:-shift_by]
+			result = [1 if ((row_copy[i] != 0 and row_shifted[i] != 0) and (row_copy[i] == row_shifted[i])) else 0 for i in range(min(len(row_copy), len(row_shifted)))]
+			# where result is 1 make self.assignments_before_search[(i, m)] = 1
+			for m, value in enumerate(result):
+				if value == 1:
+					self.assignments_before_search[(i, m)] = 1
+     
+			# simple spaces	
+			# simple spaces	 is a method explained here https://en.wikipedia.org/wiki/Nonogram
+			# it is a method which can solve parts of the puzzle before starting the search.
+			row_groups = [len(list(group)) for key, group in itertools.groupby(result) if key == 1]
+			if len(row_groups) == len(self.row_constraints[i]):
+				test = [0]*len(result)
+				group_fill = 1
+				padding_list = []
+				for j in range(len(row_groups)):
+					padding_list.append(self.row_constraints[i][j] - row_groups[j])
+				for k in range(len(row)):
+					# row = [0,1,1,0,0,0,1,1,1,0,0,0,1,0,0] becomes [0,1,1,0,0,0,2,2,2,0,0,0,3,0,0]
+					if result[k] == 1:	
+						test[k] = group_fill	
+						if k+1 < len(result) and result[k+1] == 0:
+							group_fill += 1	
+				padded_cells = self.pad_list(test, padding_list)
+				# where padded cells are 0 make self.assignments_before_search[(i, m)] = 0
+				for m, value in enumerate(padded_cells):
+					if value == 0:
+						self.assignments_before_search[(i, m)] = 0
+    	
+     	# simple boxes
+		# simple boxes is a method explained here https://en.wikipedia.org/wiki/Nonogram
+		# it is a method which can solve parts of the puzzle before starting the search.
+		for i, col in enumerate(puzzle.T):
+			index = 0
+			block = 1
+			col_copy = col.copy()
+			for j in col_constraints[i]:
+				for k in range(j):
+					col_copy[index] = block
+					index += 1
+				index += 1
+				block += 1
+			for k in range(len(col_copy)-1, -1, -1):
+				if col_copy[k] != 0:
+					shift_by = len(col_copy) - k - 1
+					break
+			# shift list row by shift_by to the right
+			col_copy = list(col_copy)
+			col_shifted = col_copy[-shift_by:] + col_copy[:-shift_by]
+
+			result = [1 if ((col_copy[i] != 0 and col_shifted[i] != 0) and (col_copy[i] == col_shifted[i])) else 0 for i in range(min(len(col_copy), len(col_shifted)))]
+			# where result is 1 make self.assignments_before_search[(m, i)] = 1
+			for m, value in enumerate(result):
+				if value == 1:
+					self.assignments_before_search[(m, i)] = 1
+     
+			# simple spaces
+			col_groups = [len(list(group)) for key, group in itertools.groupby(result) if key == 1]
+			if len(col_groups) == len(self.col_constraints[i]):
+				test = [0]*len(result)
+				group_fill = 1
+				padding_list = []
+				for j in range(len(col_groups)):
+					padding_list.append(self.col_constraints[i][j] - col_groups[j])
+				for k in range(len(col)):
+					if result[k] == 1:	
+						test[k] = group_fill	
+						if k+1 < len(result) and result[k+1] == 0:
+							group_fill += 1	
+				padded_cells = self.pad_list(test, padding_list)
+				#print("col", i)
+				#print("padded_cells", padded_cells)
+				# where padded cells are 0 make self.assignments_before_search[(m, i)] = 0
+				for m, value in enumerate(padded_cells):
+					if value == 0:
+						self.assignments_before_search[(m, i)] = 0
+
+
+		temp_puzzle = np.zeros((self.n_rows, self.n_cols))
+		for key, value in self.assignments_before_search.items():
+			if value != None:
+				temp_puzzle[key] = value
+    
+		#print(temp_puzzle)
+		# forward checking
+		for i, row in enumerate(temp_puzzle):
+			if np.sum(row) == self.row_sums[i] and super().count_groups(row) == self.row_groups[i]:
+				# assign values from temp_puzzle row to self.assignments_before_search
+				for j, value in enumerate(row):
+					self.assignments_before_search[(i, j)] = value
+		for i, col in enumerate(temp_puzzle.T):
+			if np.sum(col) == self.col_sums[i] and super().count_groups(col) == self.col_groups[i]:
+				# assign values from temp_puzzle col to self.assignments_before_search
+				for j, value in enumerate(col):
+					self.assignments_before_search[(j, i)] = value
+	
+	def print_assignment(self, assignment):
+		for row in range(self.n_rows):
+			for col in range(self.n_cols):
+				if assignment[(row, col)] == 1:
+					print(" #", end="")
+				if assignment[(row, col)] == 0:
+					print(" .", end="")
+				elif assignment[(row, col)] == None:
+					print(" x", end="")
+			print()
+		print()
+  
+	def pad_list(self, test, padding_list):
+		result = [0] * len(test)
+		for i, num in enumerate(test):
+			if num != 0:
+				padding = padding_list[num - 1]
+				for j in range(-padding, padding + 1):
+					pos = i + j
+					if 0 <= pos < len(test):
+						result[pos] = num
+		return result
+ 
+	def is_consistent(self, puzzle, assignment):
+		#row sums and groups
+		for i, row in enumerate(puzzle):
+			sum_row = np.sum(row)
+			row_groups = super().count_groups(row)
+				
+			if sum_row > self.row_sums[i]:
+				return False
+			if sum_row == self.row_sums[i] and row_groups != self.row_groups[i]:
+				return False
+			# count number of None in assignment for this row
+			none_count = 0
+			for j in range(self.n_cols):
+				if assignment[(i, j)] == None:
+					none_count += 1
+			if sum_row + none_count < self.row_sums[i]:
+				return False
+			if none_count == 0:
+				if not super().is_valid_row(row, i):
+					return False
+			#if row_groups > self.row_groups[i]:
+			#	return False
+				 
+
+		#column sums and groups
+		for i, col in enumerate(puzzle.T):
+			sum_col = np.sum(col)
+			col_groups = super().count_groups(col)
+			if sum_col > self.col_sums[i]:
+				return False
+			if sum_col == self.col_sums[i] and col_groups != self.col_groups[i]:
+				return False
+			none_count = 0
+			for j in range(self.n_rows):
+				if assignment[(j, i)] == None:
+					none_count += 1
+			if sum_col + none_count < self.col_sums[i]:
+				return False
+			if none_count == 0:
+				if not super().is_valid_col(col, i):
+					return False
+			#if col_groups > self.col_groups[i]:
+			#	return False
+		return True
+
+	def start_search(self, puzzle):
+		# where dictionary is not None assign values to puzzle
+		non_determined_cells = 0
+		for key, value in self.assignments_before_search.items():
+			if value != None:
+				puzzle[key] = value
+				continue
+			non_determined_cells += 1
+		print("before starting search:")
+		self.print_assignment(self.assignments_before_search)
+		print("size of old state space = ", "{:.2e}".format(2**(self.n_cells)))
+		print("non_determined_cells", non_determined_cells)
+		print("size of new state space = ", "{:.2e}".format(2**(non_determined_cells)))
+		print("------------------------")
+		return self.backtracking_search(puzzle, self.assignments_before_search)
+
+	def forward_checking(self, puzzle, assignment):
+		for i, row in enumerate(puzzle):
+			if np.sum(row) == self.row_sums[i] and super().count_groups(row) == self.row_groups[i]:
+				# assign values from temp_puzzle row to self.assignment
+				for j, value in enumerate(row):
+					assignment[(i, j)] = value
+		for i, col in enumerate(puzzle.T):
+			if np.sum(col) == self.col_sums[i] and super().count_groups(col) == self.col_groups[i]:
+				# assign values from temp_puzzle col to self.assignment
+				for j, value in enumerate(col):
+					assignment[(j, i)] = value
+		return assignment
+
+
+	def backtracking_search(self, puzzle, assignment):
+		# if assignment is complete then return assignment
+		if None not in assignment.values():
+			if super().is_valid(puzzle):
+				return assignment
+			#print("invalid solution, backtracking...")
+			return False
+
+  		# select unassigned variable
+		# current_cell = position
+		# select next key in dictionary which has a value of None
+		for key, value in assignment.items():
+			if value == None:
+				current_cell = key
+				#print("current cell = ", current_cell)
+				break
+		# pick a random cell in assignment which has a value of None
+		#current_cell = random.choice([key for key, value in assignment.items() if value == None])
+
+		self.states_visited += 1
+
+		if self.states_visited % 100000 == 0:
+			self.print_assignment(assignment)
+			print("states visited: ", self.states_visited)
+			print("current cell = ", current_cell)
+			print("\n\n")
+  
+
+		# Order domain values
+		values_list = [1, 0]
+		#random.shuffle(values_list)
+		#values_list = random.choices([[1, 0], [0, 1]], weights=[0.50, 0.50])[0]
+		for i in values_list:
+			temp_puzzle = puzzle.copy()
+			temp_puzzle[current_cell] = i
+			assignment[current_cell] = i
+			if self.is_consistent(temp_puzzle, assignment):
+				assignment[current_cell] = i
+				result = self.backtracking_search(temp_puzzle, assignment)
+				if result != False:
+					return result
+				assignment[current_cell] = None
+		#print("backtracking...")
+		return False
+
+
+
+
+
+
+
+
+
+
 def test_solver(solver, row_constraints, col_constraints):
 	puzzle = np.zeros((len(row_constraints), len(col_constraints)))
 	start_time = time.time()	
@@ -753,13 +1054,13 @@ if __name__ == '__main__':
 	#col_constraints = [[3,1],[3,2],[1,5,3],[3,5,1],[2,5],[2,6],[3],[3,4],[4,4],[5,4],[1,6,1,3],[1,5,3],[2,3,2],[7,2],[6,2]]
 	
 	# 10x10
-	#row_constraints = [[4,2], [2], [2,4], [1,2], [7], [3,2], [1,3,2], [2,3], [3,2], [3,2]]
-	#col_constraints = [[2], [1,1], [3], [3,1], [1,6], [6], [1,8], [1,1,1], [5,2], [5,2]]
+	row_constraints = [[4,2], [2], [2,4], [1,2], [7], [3,2], [1,3,2], [2,3], [3,2], [3,2]]
+	col_constraints = [[2], [1,1], [3], [3,1], [1,6], [6], [1,8], [1,1,1], [5,2], [5,2]]
  
-	row_constraints =  [[2] ,[1, 2] ,[4] ,[5] ,[5] ,[7] ,[1, 3] ,[2, 3] ,[3, 3] ,[4, 5]]
-	col_constraints = [[9], [4, 3], [4, 2], [4, 1], [4], [2, 5], [2, 5], [3], [1], [1]]
+	#row_constraints =  [[2] ,[1, 2] ,[4] ,[5] ,[5] ,[7] ,[1, 3] ,[2, 3] ,[3, 3] ,[4, 5]]
+	#col_constraints = [[9], [4, 3], [4, 2], [4, 1], [4], [2, 5], [2, 5], [3], [1], [1]]
 	
-	# 15x15
+	# 15x15 (code does not handle that)
 	#row_constraints =  [[4, 5] ,[3, 3] ,[1, 3, 2] ,[10] ,[10] ,[7, 7] ,[6, 3, 1] ,[6, 2] ,[4, 1] ,[3] ,[1] ,[3] ,[1, 5] ,[2, 8] ,[4, 8]]
 	#col_constraints = [[3, 1] ,[3, 2] ,[1, 5, 3] ,[3, 5, 1] ,[2, 5] ,[2, 6 ] ,[3] ,[3,4] ,[4,4] ,[5, 4] ,[1, 6, 1, 3] ,[1, 5, 3] ,[2, 3, 2] ,[7, 2] ,[6, 2]]
  
@@ -767,9 +1068,9 @@ if __name__ == '__main__':
  
 	#test_solver(BF, row_constraints, col_constraints)
  
-	test_solver(backtracking_with_prechecking, row_constraints, col_constraints)
+	#test_solver(backtracking_with_prechecking, row_constraints, col_constraints)
  
-	#test_solver(forward_checking, row_constraints, col_constraints) # does not work
+	test_solver(forward_checking_attempt_2, row_constraints, col_constraints) # does not work
 
 
 	
